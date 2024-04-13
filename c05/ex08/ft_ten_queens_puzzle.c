806dd2d6d711432831608a1575b00a7d5a40e75d
gustavovenerando <gvhenrique@hotmail.com>
2024-04-13 20:43:22 -0300

    queens puzzle finished

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -1,57 +1,76 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int ft_ten_queens_puzzle(void){
-    return 0;
+void print_board(int *board){
+    int i = 0;
+    char c;
+
+    while(i < 10){
+        c = *(board + i) + '0';
+        write(1, &c, 1);
+        i++;
+    }
+    write(1, "\n", 1);
 }
 
-int recursive(int x, int y){
-    return 0;
+int pos_diff(int a, int b){
+    if(a >= b)
+        return a - b;
+    else 
+        return b - a;
 }
 
-int placement_validation(char board[10][10], int x, int y){
-    int upper_diagonals = 1;
-    int lower_diagonals = 1;
-    for(int pos = 0; pos < 10; pos++) {
-        //Horizontal and Vertical validation
-        if (board[x][pos] == 'Q' || board[pos][y] == 'Q')
+int placement_validation(int n_queen, int *board){
+    int i = 0;
+
+    int curr_queen_col = board[n_queen];
+    while(i < n_queen){
+        int queen_to_compare_col = board[i];
+
+        int row_diff = n_queen - i; 
+        int col_diff = pos_diff(curr_queen_col, queen_to_compare_col);
+
+        if(
+            curr_queen_col == queen_to_compare_col ||
+            row_diff == col_diff
+        ){
             return 0;
-
-        int upper_x = x + pos;
-        int lower_x = x - pos;
-
-        //Upper Diagonals
-        int upper_y = y + pos;
-        if(upper_y <= 9 && upper_x <= 9 && lower_x >= 0){
-            if (board[upper_x][upper_y] == 'Q' || board[lower_x][upper_y] == 'Q')
-                upper_diagonals = 0;
         }
 
-        //Lower Diagonals
-        int lower_y = y - pos;
-        if(lower_y >= 0 && lower_x >= 0 && upper_x <= 9){
-            if (board[lower_x][lower_y] == 'Q' || board[upper_x][lower_y] == 'Q')
-                lower_diagonals = 0;
-        }
+        i++;
     }
 
-    return upper_diagonals*lower_diagonals;
+    return 1;
+}
+
+void start(int *board, int n_queen, int *count){
+    board[n_queen] = 0;
+
+    while(board[n_queen] < 10){
+        if(placement_validation(n_queen, board)){
+            if(n_queen == 9){
+                print_board(board);
+                *count += 1;
+            }
+            else
+                start(board, n_queen + 1, count);
+        }
+        board[n_queen]++;
+    }
+}
+
+int ft_ten_queens_puzzle(void){
+    int board[10];
+    int count = 0;
+    int n_queen = 0;
+
+    start(board, n_queen, &count);
+
+    return count;
 }
 
 int main(){
-    char chess_board[10][10];
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            if(i == 7 && j == 2) chess_board[i][j] = 'Q';
-            else chess_board[i][j] = '.';
-            printf("%c ", chess_board[i][j]);
-        }
-        printf("\n");
-    }
-
-    int can_place = placement_validation(chess_board, 6 , 3);
-    printf("Can place value: %d\n", can_place);
-
+    int x = ft_ten_queens_puzzle();
+    printf("Result: %d\n", x);
     return 0;
 }
