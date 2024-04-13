c584d73617e21419bafc93d685609f4a1db2a1ca
gustavovenerando <gvhenrique@hotmail.com>
2024-04-13 18:44:26 -0300

    ten queens adjust

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -5,40 +5,32 @@ int ft_ten_queens_puzzle(void){
     return 0;
 }
 
-int placement_validation(char board[10][10], int x, int y){
-    //Horizontal and Vertical validation
-    for(int pos = 0; pos < 10; pos++) {
-        if (board[x][pos] == 'Q' || board[pos][y] == 'Q')
-            return 0;
-    }
+int recursive(int x, int y){
+    return 0;
+}
 
+int placement_validation(char board[10][10], int x, int y){
     int upper_diagonals = 1;
     int lower_diagonals = 1;
     for(int pos = 0; pos < 10; pos++) {
+        //Horizontal and Vertical validation
+        if (board[x][pos] == 'Q' || board[pos][y] == 'Q')
+            return 0;
+
         int upper_x = x + pos;
         int lower_x = x - pos;
 
         //Upper Diagonals
         int upper_y = y + pos;
-        if(upper_y <= 9 && upper_x <= 9){
-            if (board[upper_x][upper_y] == 'Q')
-                upper_diagonals = 0;
-        }
-
-        if(upper_y <= 9 && lower_x >= 0){
-            if (board[lower_x][upper_y] == 'Q')
+        if(upper_y <= 9 && upper_x <= 9 && lower_x >= 0){
+            if (board[upper_x][upper_y] == 'Q' || board[lower_x][upper_y] == 'Q')
                 upper_diagonals = 0;
         }
 
         //Lower Diagonals
         int lower_y = y - pos;
-        if(lower_y >= 0 && lower_x >= 0){
-            if (board[lower_x][lower_y] == 'Q')
-                lower_diagonals = 0;
-        }
-
-        if(lower_y >= 0  && upper_x <= 9){
-            if (board[upper_x][lower_y] == 'Q')
+        if(lower_y >= 0 && lower_x >= 0 && upper_x <= 9){
+            if (board[lower_x][lower_y] == 'Q' || board[upper_x][lower_y] == 'Q')
                 lower_diagonals = 0;
         }
     }
@@ -58,7 +50,7 @@ int main(){
         printf("\n");
     }
 
-    int can_place = placement_validation(chess_board, 1 , 3);
+    int can_place = placement_validation(chess_board, 6 , 3);
     printf("Can place value: %d\n", can_place);
 
     return 0;
